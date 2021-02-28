4302dca642481655c89fb03c36aaee9603960923
aliansyahh <bb457640@gmail.com>
2021-02-28 20:11:23 +0700

    integrasi template admin lte

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+require_once "template/header.php";
+require_once "template/sidebar.php";
+?>
+
+<div class="conten-wrapper">
+    <h1>Hello World</h1>
+</div>
+
+
+<?php require_once "template/footer.php"; ?>
